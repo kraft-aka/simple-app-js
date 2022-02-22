ea169bbc8e3d59526929d1a405f0fa3ec133705b
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-02-22 14:14:45 +0100

    add objects to pokemonlist

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-let pokemonList = [];
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Wartortle',
+    height: 10,
+    type: ['water']
+  },
+  {
+    name: 'Charmander',
+    height: 6,
+    type: ['fire']
+  },
+  {
+    name: 'Pikachu',
+    height: 4,
+    type: ['electric']
+  }
+
+];
