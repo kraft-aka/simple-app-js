93be8097c8871f0839d48f8d8a310f7e29f937b0
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-03-01 22:09:46 +0100

    update some functionality in code

--- js/scripts.js
+++ js/scripts.js
@@ -29,13 +29,41 @@ let pokemonRepository = (function() {
     return pokemonList;
   }
 
-  function add(pokemon) {
-    pokemonList.push(pokemon);
+  function getLength() {
+    return pokemonList.length;
   }
 
+// function to add pokemopn to pokemonList and check the type and object keys
+  function addv(pokemon) {
+    if (typeof pokemon ==='object' && typeof pokemon!== null && Object.keys(pokemon).every(el =>['name','height', 'type'].includes(el))) {
+      pokemonList.push(pokemon);
+    } else {
+      alert('Invalid data is given.');
+    }
+  }
+
+//function to filter the pokemons to find the by name
+  const input = document.querySelector('#find');
+  input.addEventListener('input', updateValue);
+
+  function updateValue(e) {
+    let name = e.target.value;
+    let filtered = pokemonList.filter(function(pokemon){
+    if (pokemon.name === name){
+      return true;
+    } else {
+      return false;
+    }
+  });
+  console.log(filtered);
+}
+
+
   return {
-    add: add,
-    getAll: getAll
+    // add: add,
+    addv: addv,
+    getAll: getAll,
+    getLength: getLength
   }
 
 })();
@@ -49,3 +77,9 @@ pokemonRepository.getAll().forEach(function(pokemon){
     document.write('<p>' +'+ Wow, that\'s big!' + '</p>');
   }
 });
+
+
+
+// test
+console.log(pokemonRepository.addv({name:'Charmander', height: 0.6, type: ['fire']}));
+document.write('The number of pokemons are: ' + pokemonRepository.getLength());
