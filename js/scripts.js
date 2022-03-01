56a9c893ff7947536f7854ac0130d0dd3e9e615a
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-03-01 10:21:23 +0100

    initialize IIFE, update for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,49 @@
 // alert('Hello user. Welcome to the snall app!');
 
 //array of objects for pokemons with 4 items
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass', 'poison']
-  },
-  {
-    name: 'Wartortle',
-    height: 1.0,
-    type: ['water']
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-  },
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    type: ['electric']
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Wartortle',
+      height: 1.0,
+      type: ['water']
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      type: ['fire', 'flying']
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      type: ['electric']
+    }
+  ];
+
+  function getAll() {
+    return pokemonList;
   }
 
-];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  }
+
+})();
+
 
 // looping through the array objects to print all items.
 
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
   document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') '+ '</p>' );
   if (pokemon.height > 1) {
     document.write('<p>' +'+ Wow, that\'s big!' + '</p>');
